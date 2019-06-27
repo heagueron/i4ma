9c5bfdc6ff67eeaacc62f255719030f91a965a83
Héctor Agüero <heagueron@gmail.com>
2019-06-27 12:37:49 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   movie: {
     url: 'https://api.themoviedb.org/3',
-    apiKey: '2f1ad1a8e8aabbd7f23d97e90e7b2afd',
+    apiKey: 'so, you are a curious guy eh? ',
     imgPath: 'https://image.tmdb.org/t/p'
   }
 
